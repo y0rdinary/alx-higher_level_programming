7d72b0d3507bfc3b8089bbd2e9b0d82079b9d2e7
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-09-14 01:40:27 -0700

    if else loops and functions

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+        '''
+    CHecks whether charcater passed to it is lowercase or uppercaase
+    '''
+            if ord(c) >= 97 and ord(c) <= 122:
+                        return True
+                        return False
