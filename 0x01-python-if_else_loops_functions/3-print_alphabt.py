bf209013917658ba38e7d220d0bebae92799a0d0
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-10-07 09:02:39 -0700

    if else loops

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(ord('a'), ord('z')+1):
-        if alphabet in [101, 113]:
-                    continue
-                    print("{:c}".format(alphabet), end="")
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+       print("{}".format(chr(letter)), end="")
