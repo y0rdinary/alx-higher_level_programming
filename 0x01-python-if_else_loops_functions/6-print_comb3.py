eff8c9f88e6c478a2dfb35d45caeba52c86a6436
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-10-09 04:26:45 -0700

    if else loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
 
-for i in range(0, 8):
-    for c in range(n, 10):
-        print("{:d}{:d}".format(i, c), end=", ")
-    n = n + 1
-    print("89")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+    print("{}{}".format(digit1, digit2))
+        else:
+    print("{}{}".format(digit1, digit2), end=", ")
