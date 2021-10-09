9d918d48704e976a52ab239583bae09e516aea2b
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-10-09 04:22:47 -0700

    if else loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 
 for i in range(0, 8):
     for c in range(n, 10):
-      print("{:d}{:d}".format(i, c), end=", ")
+        print("{:d}{:d}".format(i, c), end=", ")
     n = n + 1
-print("89")
+    print("89")
