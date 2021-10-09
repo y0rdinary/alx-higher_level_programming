83d05fd2d0a2a1573c5581d65bcd83ccff383d0d
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-10-09 03:58:01 -0700

    if else loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(0, 100):
     if a == 99:
-         print("{:d}".format(a))
+        print("{:d}".format(a))
     else:
-         print("{:0>2d}".format(a), end="
+        print("{:0>2d}".format(a), end="
