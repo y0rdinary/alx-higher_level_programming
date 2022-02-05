5398335099171b0c72706de47b5a50490e241aaf
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-02-05 18:06:16 +0300

    js web

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-if (err) {
-console.log(err);
-} else {
-console.log(data.toString());
-}
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
