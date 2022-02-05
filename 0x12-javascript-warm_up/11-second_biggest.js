009820476514b5894d92d177ed50fb70f14136bc
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-02-05 15:39:25 +0300

    JS warmup

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let nextMax = 0;
-const args = process.argv.slice(2);
-if (args.length > 1) {
-  args.sort();
-  nextMax = args[args.length - 2];
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const numbers = process.argv.slice(2);
+  numbers.sort((a, b) => (a - b));
+  console.log(numbers[numbers.length - 2]);
 }
-console.log(nextMax);
