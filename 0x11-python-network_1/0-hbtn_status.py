88c43feac0a2d8ae0ddaadccc6d3d88bcfc8365f
root <84769643+y0rdinary@users.noreply.github.com>
2022-02-05 22:24:55 -0800

    py net

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Fetches the url https://intranet.hbtn.io/status."""
-import urllib.request
+"""
+fetches https://intranet.hbtn.io/status
+"""
 
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
