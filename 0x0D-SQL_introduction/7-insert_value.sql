03721a9562dcc349039528ca4c157a8ef8f9c5de
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-02-07 13:13:28 +0300

    sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- add new row
--- add new row
-INSERT INTO first_table (id,name) VALUES (89,'Holberton School');
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
