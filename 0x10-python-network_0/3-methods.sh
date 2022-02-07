b0164c6023c261fd07d6c57fac81f06c43dbcd0a
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-02-07 02:54:20 -0800

    py net

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display methods
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2
+# curl to display all HTTP methods the server accepts
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
